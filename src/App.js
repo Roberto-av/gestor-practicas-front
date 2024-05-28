966fd9909502f7e03f1067ec785a1f4aa6ec24c4
Roberto Aviles <avilesrobertoe@gmail.com>
2024-05-28 08:06:50 -0700

    Act App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AdminLayout from "./components/admin/layout/AdminLayout";
 import StudentLayout from "./components/student/layout/StudentLayout";
-import Users from "./pages/admin/dashboard/users";
+//import Users from "./pages/admin/dashboard/users";
+import Students from "./pages/admin/dashboard/students";
 import Home from "./pages/admin/dashboard/home";
+import { AuthProvider } from "./context/AuthContext";
+import Login from "./pages/common/login";
+import PrivateRoute from "./components/common/auth/PrivateRoute";
+import Groups from "./pages/admin/dashboard/groups";
+import GroupDetails from "./pages/admin/dashboard/groups/details";
+import StudentRegistrationPage from "./pages/student/auth/StudentRegistrationPage";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/admin/dashboard" element={<AdminLayout />}>
-          <Route index element={<Home />} />
-          <Route path="users" element={<Users />} />
-        </Route>
-        <Route path="/" element={<StudentLayout />}></Route>
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route element={<PrivateRoute />}>
+            <Route path="/admin/dashboard" element={<AdminLayout />}>
+              <Route path="students" element={<Students />} />
+              <Route path="groups" element={<Groups />} />
+              <Route path="group/:id" element={<GroupDetails />} />
+              <Route path="" element={<Home />} />
+            </Route>
+          </Route>
+          <Route path="/" element={<StudentLayout />}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/auth/register/student" element={<StudentRegistrationPage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
