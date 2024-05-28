df7eff789913ede2e67ea4da5584f9e4a0211d48
Roberto Aviles <avilesrobertoe@gmail.com>
2024-05-28 08:06:24 -0700

    logout(sin estilos)

--- src/components/student/layout/StudentLayout.js
+++ src/components/student/layout/StudentLayout.js
@@ -1,11 +1,24 @@
 import { Outlet } from "react-router-dom";
-
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 function Layout() {
+  const { authState, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/');
+  };
+
   return (
     <div>
-        <h1>Hola</h1>
-      <Outlet/>
+      {authState.token && (
+        <button onClick={handleLogout}>Cerrar sesión</button>
+      )}
+      <h1>Hola</h1>
+      <Outlet />
     </div>
   );
 }
