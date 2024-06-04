1002f37bd8cd894e3629d1d8ba7a8670ac8b4577
Roberto Aviles <avilesrobertoe@gmail.com>
2024-06-03 21:48:53 -0700

    Act StudentLayout

--- src/components/student/layout/StudentLayout.js
+++ src/components/student/layout/StudentLayout.js
@@ -2,8 +2,16 @@ import { Outlet } from "react-router-dom";
 import React, { useContext } from 'react';
 import { AuthContext } from '../../../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
+import '../../../styles/student/index.css';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function Layout() {
+  const theme = createTheme({
+    typography: {
+      fontFamily: '"Montserrat", sans-serif',
+    },
+  });
+  
   const { authState, logout } = useContext(AuthContext);
   const navigate = useNavigate();
 
@@ -13,13 +21,15 @@ function Layout() {
   };
 
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+    <div className="app-student">
       {authState.token && (
         <button onClick={handleLogout}>Cerrar sesión</button>
       )}
       <h1>Hola</h1>
       <Outlet />
     </div>
+    </ThemeProvider>
   );
 }
 
