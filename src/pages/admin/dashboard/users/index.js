a24b0c12098bb4e52018064fb445c485d8abf3b2
Roberto Aviles <avilesrobertoe@gmail.com>
2024-06-03 21:48:05 -0700

    Act pagina users

--- src/pages/admin/dashboard/users/index.js
+++ src/pages/admin/dashboard/users/index.js
@@ -1,23 +1,14 @@
-import Table from "../../../../components/admin/dashboard/table";
+import Header from "../../../../components/common/header";
+import { Box } from "@mui/material";
 
 const Users = () => {
   const title = "Usuarios";
   const subtitle = "Gestionar a los estudiantes con usuario";
-  const rows = [
-    { id: 1, name: "John Doe", age: 30, phone: "123-456-7890", email: "john@example.com" },
-  ];
-  const columns = [
-    { field: "id", headerName: "ID" },
-    { field: "name", headerName: "Name", flex: 1 },
-    { field: "age", headerName: "Age", type: "number", headerAlign: "left", align: "left" },
-    { field: "phone", headerName: "Phone Number", flex: 1 },
-    { field: "email", headerName: "Email", flex: 1 },
-  ];
 
   return (
-    <div>
-      <Table title={title} subtitle={subtitle} rows={rows} columns={columns} />
-    </div>
+    <Box>
+      <Header title={title} subtitle={subtitle} />
+    </Box>
   );
 };
 
