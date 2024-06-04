a02ff47a4c21b947896b43874a44f242fcceafb9
Roberto Aviles <avilesrobertoe@gmail.com>
2024-06-03 21:44:44 -0700

    Act Pagina de regisro

--- src/pages/student/auth/StudentRegistrationPage.js
+++ src/pages/student/auth/StudentRegistrationPage.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { TextField, Button, Typography, Grid, Paper } from '@mui/material';
+import { TextField, Button, Typography, Grid, Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import api from '../../../utils/api';
 import { AuthContext } from '../../../context/AuthContext';
 import { jwtDecode } from 'jwt-decode';
+import Loader from '../../../components/admin/dashboard/loader';
 
 function StudentRegistrationPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [token, setToken] = useState('');
-  const [error, setError] = useState('');
+  const [errors, setErrors] = useState('');
   const { setAuthState } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
+  const [serverError, setServerError] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -19,15 +23,43 @@ function StudentRegistrationPage() {
     if (tokenParam) {
       setToken(tokenParam);
     } else {
-      setError('Token de registro no encontrado.');
+      setErrors('Token de registro no encontrado.');
     }
   }, []);
 
-  const handleRegister = async () => {
+  const validate = () => {
+    let tempErrors = {};
+    tempErrors.username = username ? "" : "El nombre de usuario es requerido";
+    tempErrors.password = password ? "" : "La contraseña es requerida";
+    setErrors(tempErrors);
+    return Object.values(tempErrors).every((x) => x === "");
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "username") {
+      setUsername(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+
+    if (isSubmitted) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        [name]: value ? "" : `El ${name === "username" ? "nombre de usuario" : "contraseña"} es requerido`,
+      }));
+    }
+  };
+
+  const handleSubmit = async () => {
+    setIsSubmitted(true);
     if (!token) {
-      setError('Token de registro no encontrado.');
+      setErrors('Token de registro no encontrado.');
       return;
     }
+
+    if (!validate()) return;
+    setLoading(true);
     try {
       await api.post(`/auth/register/student?token=${token}`, { username, password });
       console.log("El usuario se ha registrado");
@@ -40,50 +72,146 @@ function StudentRegistrationPage() {
         jwt,
         user: jwtDecode(jwt),
       });
-      
       navigate("/");
+      window.location.reload(true);
     } catch (error) {
-      console.error('Error al registrar al estudiante:', error);
-      setError('Error al registrar al estudiante. Por favor, inténtalo de nuevo.');
+      console.error("Error en el login:", error);
+      if (error.response && error.response.data) {
+        const serverErrors = error.response.data || "Error desconocido";
+        setServerError(serverErrors);
+      } else {
+        setServerError("Error con el servidor.");
+      }
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
-      <Grid item xs={10} sm={6} md={4} lg={3}>
-        <Paper elevation={3} style={{ padding: 20 }}>
-          <Typography variant="h5" align="center" gutterBottom>
-            Registro de Estudiante
-          </Typography>
-          <TextField
-            label="Nombre de Usuario"
-            fullWidth
-            margin="normal"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <TextField
-            label="Contraseña"
-            fullWidth
-            margin="normal"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {error && <Typography color="error" variant="body2" gutterBottom>{error}</Typography>}
-          <Button
-            variant="contained"
-            color="primary"
-            fullWidth
-            style={{ marginTop: 20 }}
-            onClick={handleRegister}
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      style={{ minHeight: "80vh" }}
+    >
+      {loading ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height="80vh"
+        >
+          <Loader />
+        </Box>
+      ) : (
+        <Grid item xs={11} sm={8} md={6} lg={4}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            p={3}
+            borderRadius={2}
           >
-            Registrarse
-          </Button>
-        </Paper>
-      </Grid>
+            <Typography variant="h4" mb={2}>
+              Registro
+            </Typography>
+            <form onSubmit={handleSubmit} style={{ width: "100%" }}>
+              <TextField
+                label="Username"
+                name="username"
+                type="text"
+                fullWidth
+                margin="normal"
+                value={username}
+                sx={{
+                  "& .MuiInputLabel-root": {
+                    color: "#0f0f0f",
+                  },
+                  "& .MuiInputLabel-outlined": {
+                    transform: "translate(14px, 14px) scale(1)",
+                  },
+                  "& .MuiInputLabel-outlined.MuiInputLabel-shrink": {
+                    transform: "translate(14px, -6px) scale(0.75)",
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderColor: "#0f0f0f",
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "#0f0f0f",
+                    },
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#0f0f0f",
+                    },
+                  },
+                }}
+                onChange={handleChange}
+                error={isSubmitted && !!errors.username}
+                helperText={isSubmitted && errors.username}
+                variant="outlined"
+              />
+              <TextField
+                label="Password"
+                name="password"
+                type="password"
+                fullWidth
+                margin="normal"
+                value={password}
+                sx={{
+                  marginBottom: "20px",
+                  "& .MuiInputLabel-root": {
+                    color: "#0f0f0f",
+                  },
+                  "& .MuiInputLabel-outlined": {
+                    transform: "translate(14px, 14px) scale(1)",
+                  },
+                  "& .MuiInputLabel-outlined.MuiInputLabel-shrink": {
+                    transform: "translate(14px, -6px) scale(0.75)",
+                  },
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderColor: "#0f0f0f",
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "#0f0f0f",
+                    },
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#0f0f0f",
+                    },
+                  },
+                }}
+                onChange={handleChange}
+                error={isSubmitted && !!errors.password}
+                helperText={isSubmitted && errors.password}
+                variant="outlined"
+              />
+              {serverError && (
+                <Typography color="error" variant="body2" mb={2}>
+                  {serverError}
+                </Typography>
+              )}
+              <Button
+                type="submit"
+                variant="contained"
+                sx={{
+                  backgroundColor: "#0F0F0F",
+                  color: "#FFFFFF",
+                  height: "65px",
+                  "&:hover": {
+                    backgroundColor: "#222222",
+                  },
+                }}
+                fullWidth
+              >
+                INGRESAR
+              </Button>
+            </form>
+          </Box>
+        </Grid>
+      )}
     </Grid>
   );
-}
+};
+
 
 export default StudentRegistrationPage;
