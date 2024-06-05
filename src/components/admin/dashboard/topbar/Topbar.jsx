bf746c6f14fd9c13ef860be9c2e8dab480e722cc
Roberto Aviles <avilesrobertoe@gmail.com>
2024-06-04 19:07:04 -0700

    Act Topbar admin

--- src/components/admin/dashboard/topbar/Topbar.jsx
+++ src/components/admin/dashboard/topbar/Topbar.jsx
@@ -1,18 +1,18 @@
 import { Box, IconButton, useTheme, Menu, MenuItem } from "@mui/material";
 import { useContext, useState } from "react";
 import { ColorModeContext, tokens } from "../../../../theme";
-import InputBase from "@mui/material/InputBase";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchIcon from "@mui/icons-material/Search";
-import LogoutIcon from '@mui/icons-material/Logout';
+import LogoutIcon from "@mui/icons-material/Logout";
 import { AuthContext } from "../../../../context/AuthContext";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import { Link } from "react-router-dom";
 
 const Topbar = () => {
-  const theme = useTheme(); 
+  const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
   const { logout } = useContext(AuthContext);
@@ -36,15 +36,16 @@ const Topbar = () => {
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
       {/* SEARCH BAR */}
-      <Box
-        display="flex"
-        backgroundColor={colors.primary[400]}
-        borderRadius="3px"
-      >
-        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+      <Box display="flex" borderRadius="3px">
+        {/* <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
         <IconButton type="button" sx={{ p: 1 }}>
           <SearchIcon />
-        </IconButton>
+        </IconButton> */}
+        <Link to="/">
+          <IconButton>
+            <HomeOutlinedIcon />
+          </IconButton>
+        </Link>
       </Box>
 
       {/* ICONS */}
